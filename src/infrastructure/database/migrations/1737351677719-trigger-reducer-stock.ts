9a2efdc1bc152050ae0d1b65947856323827bfed
Yesid Hernandez <fyesid.h@gmail.com>
2025-01-20 02:11:51 -0500

    feat: enhance payments migration with existence checks for table and columns, and improve trigger creation logic

--- src/infrastructure/database/migrations/1737351677719-trigger-reducer-stock.ts
+++ src/infrastructure/database/migrations/1737351677719-trigger-reducer-stock.ts
@@ -3,38 +3,51 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class TriggerReducerStock1737351677719 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-            CREATE OR REPLACE FUNCTION update_product_stock_after_payment()
-            RETURNS TRIGGER AS $$
-            BEGIN
-                -- Verificamos si el status del pago es 'APPROVED'
-                IF NEW.status = 'APPROVED' THEN
-                    -- Actualizamos el stock del producto correspondiente
-                    UPDATE products
-                    SET stock = stock - NEW.product_quantity
-                    WHERE id = NEW.product_id;
-                END IF;
-                RETURN NEW;
-            END;
-            $$ LANGUAGE plpgsql;
-        `);
+      CREATE OR REPLACE FUNCTION update_product_stock_after_payment()
+      RETURNS TRIGGER AS $$
+      BEGIN
+          -- Verificamos si el status del pago es 'APPROVED'
+          IF NEW.status = 'APPROVED' THEN
+              -- Actualizamos el stock del producto correspondiente
+              UPDATE products
+              SET stock = stock - NEW.product_quantity
+              WHERE id = NEW.product_id;
+          END IF;
+          RETURN NEW;
+      END;
+      $$ LANGUAGE plpgsql;
+    `);
 
     await queryRunner.query(`
-            CREATE TRIGGER trigger_update_product_stock
-            AFTER INSERT ON payments
-            FOR EACH ROW
-            EXECUTE FUNCTION update_product_stock_after_payment();
-        `);
+      DO $$
+      BEGIN
+        IF NOT EXISTS (SELECT 1 FROM pg_trigger WHERE tgname = 'trigger_update_product_stock') THEN
+          CREATE TRIGGER trigger_update_product_stock
+          AFTER INSERT ON payments
+          FOR EACH ROW
+          EXECUTE FUNCTION update_product_stock_after_payment();
+        END IF;
+      END $$;
+    `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    // Eliminar el trigger
     await queryRunner.query(`
-            DROP TRIGGER IF EXISTS trigger_update_product_stock ON payments;
-        `);
+      DO $$
+      BEGIN
+        IF EXISTS (SELECT 1 FROM pg_trigger WHERE tgname = 'trigger_update_product_stock') THEN
+          DROP TRIGGER IF EXISTS trigger_update_product_stock ON payments;
+        END IF;
+      END $$;
+    `);
 
-    // Eliminar el procedimiento almacenado
     await queryRunner.query(`
-            DROP FUNCTION IF EXISTS update_product_stock_after_payment();
-        `);
+      DO $$
+      BEGIN
+        IF EXISTS (SELECT 1 FROM pg_proc WHERE proname = 'update_product_stock_after_payment') THEN
+          DROP FUNCTION IF EXISTS update_product_stock_after_payment();
+        END IF;
+      END $$;
+    `);
   }
 }
